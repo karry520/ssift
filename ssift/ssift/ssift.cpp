2f0146989d78dc205cf4f38f9aa3c859bbc35e39
karry520 <884816926@qq.com>
2017-01-10 20:32:34 +0800

    The second step of ssift
    Add some function for second step of ssift.cpp

--- ssift/ssift/ssift.cpp
+++ ssift/ssift/ssift.cpp
@@ -390,14 +390,16 @@ static CvSeq* scale_space_extrema( IplImage*** dog_pyr, int octvs, int intvls,
 		for( i = 1; i <= intvls; i++ )
 			for(r = SSIFT_IMG_BORDER; r < dog_pyr[o][0]->height-SSIFT_IMG_BORDER; r++)
 				for(c = SSIFT_IMG_BORDER; c < dog_pyr[o][0]->width-SSIFT_IMG_BORDER; c++)
-					/* perform preliminary check on contrast */
+					/* 像素点的对比度大于阈值 */
 					if( ABS( pixval32f( dog_pyr[o][i], r, c ) ) > prelim_contr_thr )
 						if( is_extremum( dog_pyr, o, i, r, c ) )
 						{
+							/* 极值点精确定位 */
 							feat = interp_extremum(dog_pyr, o, i, r, c, intvls, contr_thr);
 							if( feat )
 							{
 								ddata = feat_detection_data( feat );
+								/* 消除边缘响应点 */
 								if( ! is_too_edge_like( dog_pyr[ddata->octv][ddata->intvl],
 									ddata->r, ddata->c, curv_thr ) )
 								{
@@ -410,4 +412,359 @@ static CvSeq* scale_space_extrema( IplImage*** dog_pyr, int octvs, int intvls,
 						}
 
 	return features;
+}
+
+/*****************************************************************************
+Function:		// is_extremum
+Description:	// 判断给定点是否是极值点（是否是周边26个点中的最大最小）
+Calls:			// pixval32f、
+Called By:		// scale_space_extrema
+Table Accessed: // 无
+Table Updated:	// 无
+Input:			// @dog_pyr		高斯差分金字塔
+				// @octvs		所在组数
+				// @intvls		所在层数
+				// @r			所在行
+				// @c			所在列
+Output:			// 无
+Return:			// 最大或最小值返回0 其它返回非0
+Others:			// 其它说明
+*****************************************************************************/
+static int is_extremum( IplImage*** dog_pyr, int octv, int intvl, int r, int c )
+{
+	float val = pixval32f( dog_pyr[octv][intvl], r, c );
+	int i, j, k;
+
+	/* 检查是否是最大值 */
+	if( val > 0 )
+	{
+		for( i = -1; i <= 1; i++ )
+			for( j = -1; j <= 1; j++ )
+				for( k = -1; k <= 1; k++ )
+					if( val < pixval32f( dog_pyr[octv][intvl+i], r + j, c + k ) )
+						return 0;
+	}
+
+	/* 检查是否是最小值 */
+	else
+	{
+		for( i = -1; i <= 1; i++ )
+			for( j = -1; j <= 1; j++ )
+				for( k = -1; k <= 1; k++ )
+					if( val > pixval32f( dog_pyr[octv][intvl+i], r + j, c + k ) )
+						return 0;
+	}
+
+	return 1;
+}
+
+
+
+/*****************************************************************************
+Function:		// interp_extremum
+Description:	// 插值拟合，极值点精确定位
+Calls:			// interp_step、interp_contr
+Called By:		// scale_space_extrema
+Table Accessed: // 无
+Table Updated:	// 无
+Input:			// @dog_pyr		高斯差分金字塔
+				// @octvs		所在组数
+				// @intvls		所在层数
+				// @r			所在行
+				// @c			所在列
+				// @intvls		金字塔每组的导数
+				// @contr_thr	对比度阈值，去除不稳定特征
+Output:			// 无
+Return:			// 返回精确定位后的特征点结构体
+Others:			// 其它说明
+*****************************************************************************/
+static struct feature* interp_extremum( IplImage*** dog_pyr, int octv, int intvl,
+										int r, int c, int intvls, double contr_thr )
+{
+	struct feature* feat;
+	struct detection_data* ddata;
+	double xi, xr, xc, contr;
+	int i = 0;
+
+	while( i < SSIFT_MAX_INTERP_STEPS )
+	{
+		interp_step( dog_pyr, octv, intvl, r, c, &xi, &xr, &xc );
+		if( ABS( xi ) < 0.5  &&  ABS( xr ) < 0.5  &&  ABS( xc ) < 0.5 )
+			break;
+
+		c += cvRound( xc );
+		r += cvRound( xr );
+		intvl += cvRound( xi );
+
+		if( intvl < 1  ||
+			intvl > intvls  ||
+			c < SSIFT_IMG_BORDER  ||
+			r < SSIFT_IMG_BORDER  ||
+			c >= dog_pyr[octv][0]->width - SSIFT_IMG_BORDER  ||
+			r >= dog_pyr[octv][0]->height - SSIFT_IMG_BORDER )
+		{
+			return NULL;
+		}
+
+		i++;
+	}
+
+	/* ensure convergence of interpolation */
+	if( i >= SSIFT_MAX_INTERP_STEPS )
+		return NULL;
+
+	contr = interp_contr( dog_pyr, octv, intvl, r, c, xi, xr, xc );
+	if( ABS( contr ) < contr_thr / intvls )
+		return NULL;
+
+	feat = new_feature();
+	ddata = feat_detection_data( feat );
+	feat->img_point.x = feat->x = ( c + xc ) * pow( 2.0, octv );
+	feat->img_point.y = feat->y = ( r + xr ) * pow( 2.0, octv );
+	ddata->r = r;
+	ddata->c = c;
+	ddata->octv = octv;
+	ddata->intvl = intvl;
+	ddata->subintvl = xi;
+
+	return feat;
+}
+
+/*****************************************************************************
+Function:		// interp_step
+Description:	// 每一步拟合的操作
+Calls:			// deriv_3D、hessian_3D、cvCreateMat、cvInvert、cvGEMM、cvReleaseMat
+Called By:		// interp_extremum
+Table Accessed: // 无
+Table Updated:	// 无
+Input:			// @dog_pyr		高斯差分金字塔
+				// @octvs		所在组数
+				// @intvls		所在层数
+				// @r			所在行
+				// @c			所在列
+				// @xi			像素点层方向上的偏移量
+				// @xr			像素点行方向上的偏移量
+				// @xc			像素点列方向上的偏移量
+Output:			// 无
+Return:			// 返回三维方向上的偏移量
+Others:			// 其它说明
+*****************************************************************************/
+
+static void interp_step( IplImage*** dog_pyr, int octv, int intvl, int r, int c,
+						 double* xi, double* xr, double* xc )
+{
+	CvMat* dD, * H, * H_inv, X;
+	double x[3] = { 0 };
+
+	dD = deriv_3D( dog_pyr, octv, intvl, r, c );
+	H = hessian_3D( dog_pyr, octv, intvl, r, c );
+	H_inv = cvCreateMat( 3, 3, CV_64FC1 );
+	cvInvert( H, H_inv, CV_SVD );
+	cvInitMatHeader( &X, 3, 1, CV_64FC1, x, CV_AUTOSTEP );
+	cvGEMM( H_inv, dD, -1, NULL, 0, &X, 0 );
+
+	cvReleaseMat( &dD );
+	cvReleaseMat( &H );
+	cvReleaseMat( &H_inv );
+
+	*xi = x[2];
+	*xr = x[1];
+	*xc = x[0];
+}
+
+
+
+/*****************************************************************************
+Function:		// deriv_3D
+Description:	// 每一步拟合的操作计算高斯差像素点在行、列、层三个方向上的偏导
+Calls:			// pixval32f、cvCreateMat、cvmSet
+Called By:		// interp_step
+Table Accessed: // 无
+Table Updated:	// 无
+Input:			// @dog_pyr		高斯差分金字塔
+				// @octvs		所在组数
+				// @intvls		所在层数
+				// @r			所在行
+				// @c			所在列
+Output:			// 无
+Return:			// 返回由像素点在行、列、层三个方向上的偏导构成的矩阵
+Others:			// 其它说明
+*****************************************************************************/
+static CvMat* deriv_3D( IplImage*** dog_pyr, int octv, int intvl, int r, int c )
+{
+	CvMat* dI;
+	double dx, dy, ds;
+
+	dx = ( pixval32f( dog_pyr[octv][intvl], r, c+1 ) -
+		pixval32f( dog_pyr[octv][intvl], r, c-1 ) ) / 2.0;
+	dy = ( pixval32f( dog_pyr[octv][intvl], r+1, c ) -
+		pixval32f( dog_pyr[octv][intvl], r-1, c ) ) / 2.0;
+	ds = ( pixval32f( dog_pyr[octv][intvl+1], r, c ) -
+		pixval32f( dog_pyr[octv][intvl-1], r, c ) ) / 2.0;
+
+	dI = cvCreateMat( 3, 1, CV_64FC1 );
+	cvmSet( dI, 0, 0, dx );
+	cvmSet( dI, 1, 0, dy );
+	cvmSet( dI, 2, 0, ds );
+
+	return dI;
+}
+
+
+/*****************************************************************************
+Function:		// hessian_3D
+Description:	// 计算像素点的3*3的海森矩阵
+Calls:			// pixval32f、cvCreateMat、cvmSet
+Called By:		// interp_step
+Table Accessed: // 无
+Table Updated:	// 无
+Input:			// @dog_pyr		高斯差分金字塔
+				// @octvs		所在组数
+				// @intvls		所在层数
+				// @r			所在行
+				// @c			所在列
+Output:			// 无
+Return:			// 返回由像素点的海森矩阵
+Others:			// 其它说明
+*****************************************************************************/
+static CvMat* hessian_3D( IplImage*** dog_pyr, int octv, int intvl, int r, int c )
+{
+	CvMat* H;
+	double v, dxx, dyy, dss, dxy, dxs, dys;
+
+	v = pixval32f( dog_pyr[octv][intvl], r, c );
+	dxx = ( pixval32f( dog_pyr[octv][intvl], r, c+1 ) + 
+			pixval32f( dog_pyr[octv][intvl], r, c-1 ) - 2 * v );
+	dyy = ( pixval32f( dog_pyr[octv][intvl], r+1, c ) +
+			pixval32f( dog_pyr[octv][intvl], r-1, c ) - 2 * v );
+	dss = ( pixval32f( dog_pyr[octv][intvl+1], r, c ) +
+			pixval32f( dog_pyr[octv][intvl-1], r, c ) - 2 * v );
+	dxy = ( pixval32f( dog_pyr[octv][intvl], r+1, c+1 ) -
+			pixval32f( dog_pyr[octv][intvl], r+1, c-1 ) -
+			pixval32f( dog_pyr[octv][intvl], r-1, c+1 ) +
+			pixval32f( dog_pyr[octv][intvl], r-1, c-1 ) ) / 4.0;
+	dxs = ( pixval32f( dog_pyr[octv][intvl+1], r, c+1 ) -
+			pixval32f( dog_pyr[octv][intvl+1], r, c-1 ) -
+			pixval32f( dog_pyr[octv][intvl-1], r, c+1 ) +
+			pixval32f( dog_pyr[octv][intvl-1], r, c-1 ) ) / 4.0;
+	dys = ( pixval32f( dog_pyr[octv][intvl+1], r+1, c ) -
+			pixval32f( dog_pyr[octv][intvl+1], r-1, c ) -
+			pixval32f( dog_pyr[octv][intvl-1], r+1, c ) +
+			pixval32f( dog_pyr[octv][intvl-1], r-1, c ) ) / 4.0;
+
+	H = cvCreateMat( 3, 3, CV_64FC1 );
+	cvmSet( H, 0, 0, dxx );
+	cvmSet( H, 0, 1, dxy );
+	cvmSet( H, 0, 2, dxs );
+	cvmSet( H, 1, 0, dxy );
+	cvmSet( H, 1, 1, dyy );
+	cvmSet( H, 1, 2, dys );
+	cvmSet( H, 2, 0, dxs );
+	cvmSet( H, 2, 1, dys );
+	cvmSet( H, 2, 2, dss );
+
+	return H;
+}
+
+
+
+/*****************************************************************************
+Function:		// interp_contr
+Description:	// 计算被插值后像素点的对比度
+Calls:			// cvInitMatHeader、deriv_3D、cvGEMM、cvReleaseMat、pixval32f
+Called By:		// scale_space_extrema
+Table Accessed: // 无
+Table Updated:	// 无
+Input:			// @dog_pyr		高斯差分金字塔
+				// @octvs		所在组数
+				// @intvls		所在层数
+				// @r			所在行
+				// @c			所在列
+				// @xi			像素点层方向上的偏移量
+				// @xr			像素点行方向上的偏移量
+				// @xc			像素点列方向上的偏移量
+Output:			// 无
+Return:			// 返回由像素点的海森矩阵
+Others:			// 其它说明
+*****************************************************************************/
+static double interp_contr( IplImage*** dog_pyr, int octv, int intvl, int r,
+							int c, double xi, double xr, double xc )
+{
+	CvMat* dD, X, T;
+	double t[1], x[3] = { xc, xr, xi };
+
+	cvInitMatHeader( &X, 3, 1, CV_64FC1, x, CV_AUTOSTEP );
+	cvInitMatHeader( &T, 1, 1, CV_64FC1, t, CV_AUTOSTEP );
+	dD = deriv_3D( dog_pyr, octv, intvl, r, c );
+	cvGEMM( dD, &X, 1, NULL, 0, &T,  CV_GEMM_A_T );
+	cvReleaseMat( &dD );
+
+	return pixval32f( dog_pyr[octv][intvl], r, c ) + t[0] * 0.5;
+}
+
+
+
+/*****************************************************************************
+Function:		// new_feature
+Description:	// 创建一个新的特征点(feature)结构体
+Calls:			// cvInitMatHeader、deriv_3D、cvGEMM、cvReleaseMat、pixval32f
+Called By:		// scale_space_extrema
+Table Accessed: // 无
+Table Updated:	// 无
+Input:			// 无
+Output:			// 无
+Return:			// 返回已分配存储空间的结构体首地址
+Others:			// 其它说明
+*****************************************************************************/
+static struct feature* new_feature( void )
+{
+	struct feature* feat;
+	struct detection_data* ddata;
+
+	feat = (feature*)malloc( sizeof( struct feature ) );
+	memset( feat, 0, sizeof( struct feature ) );
+	ddata = (detection_data*)malloc( sizeof( struct detection_data ) );
+	memset( ddata, 0, sizeof( struct detection_data ) );
+	feat->feature_data = ddata;
+
+	return feat;
+}
+
+
+
+/*****************************************************************************
+Function:		// is_too_edge_like
+Description:	// 消除不稳定的边缘响应点
+Calls:			// pixval32f、
+Called By:		// scale_space_extrema
+Table Accessed: // 无
+Table Updated:	// 无
+Input:			// @dog_pyr		高斯差分金字塔
+				// @r			所在行
+				// @c			所在列
+				// @curv_thr	主曲率比值
+Output:			// 
+Return:			// 0：非边缘点 1：边缘点
+Others:			// 其它说明
+*****************************************************************************/
+static int is_too_edge_like( IplImage* dog_img, int r, int c, int curv_thr )
+{
+	double d, dxx, dyy, dxy, tr, det;
+
+	/* 主曲率与海森矩阵的特征值成正比 */
+	d = pixval32f(dog_img, r, c);
+	dxx = pixval32f( dog_img, r, c+1 ) + pixval32f( dog_img, r, c-1 ) - 2 * d;
+	dyy = pixval32f( dog_img, r+1, c ) + pixval32f( dog_img, r-1, c ) - 2 * d;
+	dxy = ( pixval32f(dog_img, r+1, c+1) - pixval32f(dog_img, r+1, c-1) -
+			pixval32f(dog_img, r-1, c+1) + pixval32f(dog_img, r-1, c-1) ) / 4.0;
+	tr = dxx + dyy;
+	det = dxx * dyy - dxy * dxy;
+
+	/* 与主曲率有不同的符号 */
+	if( det <= 0 )
+		return 1;
+	/* 通过比值判断 */
+	if( tr * tr / det < ( curv_thr + 1.0 )*( curv_thr + 1.0 ) / curv_thr )
+		return 0;
+	return 1;
 }
